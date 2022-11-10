71c2f2a89cb45230d1c383f9784a2515d846e22f
Marcus Walker <mwalke35@yahoo.com>
2022-11-10 00:20:21 -0500

    added javascript code and used assignments by ID and functions and array for the password generator

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-// Assignment code here
+// Assignment code here pulling from the ID from the HTML code
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+const generatePasswordButtonEl = document.getElementById('generate')
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//added variables for strings so they can be used in an array for the random generator
+const UPPERCASE_CHAR_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const LOWERCASE_CHAR_CODES = "abcdefghijklmnopqrstuvwxyz"
+const NUMBER_CHAR_CODES = "0123456789"
+const SYMBOL_CHAR_CODES = "!@#$%^&*()"
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const upperCaseArray = UPPERCASE_CHAR_CODES.split("");
+const lowerCaseArray = LOWERCASE_CHAR_CODES.split("");
+const numberArray = NUMBER_CHAR_CODES.split("");
+const symbolArray = SYMBOL_CHAR_CODES.split("");
 
-  passwordText.value = password;
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
+})
+//concat the array of code together 
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = lowerCaseArray
+  if (includeUppercase) {charCodes = charCodes.concat(upperCaseArray)}
+  if (includeSymbols) {charCodes = charCodes.concat(symbolArray)}
+  if (includeNumbers) {charCodes = charCodes.concat(numberArray)}
+  //console.log(charCodes)
+
+// generates the random characters to passwordCharacters push to the array
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(characterCode)
+  }
+  return passwordCharacters.join('')
+}
+//slider and number function to match 
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
